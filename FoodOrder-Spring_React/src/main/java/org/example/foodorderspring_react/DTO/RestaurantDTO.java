c9ca1ca2e2f82f1cc7a2e3b64d8ad83d27982e30
rl <74465003+ramone731@users.noreply.github.com>
2024-07-19 23:06:54 -0400

    Update RestaurantDTO.java

--- FoodOrder-Spring_React/src/main/java/org/example/foodorderspring_react/DTO/RestaurantDTO.java
+++ FoodOrder-Spring_React/src/main/java/org/example/foodorderspring_react/DTO/RestaurantDTO.java
@@ -1,13 +1,20 @@
-package org.example.foodorderspring_react.DTO;
+package org.example.rl.foodorderspring_reactv6.DTO;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Embeddable;
+import lombok.Data;
 
 import java.util.List;
 
+@Data
+@Embeddable
 public class RestaurantDTO {
     private String title;
+
     @Column(length = 1000)
-    private List<String> images;
+    @ElementCollection /*syntax - Basic attribute should not be a container*/
+    private List<String> image;
 
     private String description;
     private Long id;
