95fbad32232eb2532d7eca9e3d9f810f0112c921
rl <74465003+ramone731@users.noreply.github.com>
2024-07-19 23:05:46 -0400

    Add files via upload

---
+++ FoodOrder-Spring_React/src/main/java/org/example/foodorderspring_react/Model/Order.java
@@ -0,0 +1,41 @@
+package org.example.rl.foodorderspring_reactv6.Model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    private User customer;
+
+    @JsonIgnore
+    @ManyToOne
+    private Restaurant restaurant;
+
+    private Long totalAmount;
+    private String orderStatus;
+
+    private Date createdAt;
+
+    @ManyToOne
+    private Address deliveryAddress;
+
+    @OneToMany // in 1 order user can add multiple order
+    private List<OrderItem> items;
+    private int totalItem;
+    private int totalPrice;
+}
