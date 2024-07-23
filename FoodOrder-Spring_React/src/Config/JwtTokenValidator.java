7c48b16791cc0a33f7d413cba4ccc26e3249349f
rl <74465003+ramone731@users.noreply.github.com>
2024-07-23 12:45:05 -0400

    Add files via upload

---
+++ FoodOrder-Spring_React/src/Config/JwtTokenValidator.java
@@ -0,0 +1,14 @@
+package org.example.rl.foodorderspring_reactv6.Config;
+
+import jakarta.servlet.*;
+import java.io.IOException;
+
+public class JwtTokenValidator implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+
+    }
+}
