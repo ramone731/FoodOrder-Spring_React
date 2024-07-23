fbde1b693e7e12abd507d3ac7a2258c1e82c7375
rl <74465003+ramone731@users.noreply.github.com>
2024-07-23 13:34:09 -0400

    Delete FoodOrder-Spring_React/src/main directory

--- FoodOrder-Spring_React/src/main/java/org/example/foodorderspring_react/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.rl.foodorderspring_reactv6.Repository;
-
-import org.example.rl.foodorderspring_reactv6.Model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    public User findByEmail(String username);
-}
